0370611d768c68e318a4bbb881ac818c3b41a610
John <jddhalogamer@gmail.com>
2021-10-29 09:34:39 -0500

    added About component, and BrowserRouter to app.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
             <nav>
-                <h4>
-                    <Link to='/'><i></i>Home</Link>
-                </h4>
+                    <Router>
+                    <Link to="/about">About</Link>
+                    </Router>
             </nav>
     )
 }
